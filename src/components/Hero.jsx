bae787e972396f40805c58dbb58cfd619477b130
hadijmldn <hadijmldn@gmail.com>
2023-03-16 20:07:40 +0700

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute
       inset-0 top-[120px] max-2-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-5 h-5 rounded-full bg-[#1d82e0]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Hadi</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#4aa7f3]">Hadi</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               I'm Web Developer, User <br className='sm:block hidden' /> interfaces, and FiveM Developer
           </p>
